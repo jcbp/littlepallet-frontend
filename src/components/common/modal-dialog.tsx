a23a3d9e3ba267a12f70c9337eae9e7044804577
Carlos Bertinci <cbertinci@vi-datec.com>
2023-05-27 23:14:00 -0300

    fix: clipping dropdown options in item-view

--- src/components/common/modal-dialog.tsx
+++ src/components/common/modal-dialog.tsx
@@ -51,7 +51,7 @@ const ModalDialog: React.FC<ModalProps> = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6 lg:max-w-xl">
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6 lg:max-w-xl">
               <div className="absolute top-0 right-0 pt-4 pr-4">
                 <button
                   className="text-gray-500 hover:text-gray-700 transition-all duration-200"
