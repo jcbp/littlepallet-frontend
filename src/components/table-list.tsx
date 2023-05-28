369a6c79a15cdb34bc634e13331e25b2fd3c509e
Carlos Bertinci <cbertinci@vi-datec.com>
2023-05-27 21:25:00 -0300

    feat: hide id in mobile view

--- src/components/table-list.tsx
+++ src/components/table-list.tsx
@@ -1,3 +1,4 @@
+import { useIsMobile } from "../hooks/mobile";
 import { Field } from "../types/field";
 import { Item } from "../types/item";
 import FieldView from "./field-view";
@@ -25,6 +26,7 @@ const TableList: React.FC<ListCardProps> = ({
   highlightItem,
   highlightColor,
 }) => {
+  const { isMobile } = useIsMobile();
   const formatId = (id: string) => {
     const paddedNum = String(id).padStart(3, String.fromCharCode(160)); // 160 es el código decimal para "&nbsp;"
     return paddedNum;
@@ -33,9 +35,11 @@ const TableList: React.FC<ListCardProps> = ({
     <table className="table-auto w-full">
       <thead>
         <tr>
-          <th className="border-b border-slate-200 pb-3 pl-3.5 text-left text-gray-500 w-[58px]">
-            <HashtagIcon className="h-4 w-4 text-gray-500" />
-          </th>
+          {!isMobile && (
+            <th className="border-b border-slate-200 pb-3 pl-3.5 text-left text-gray-500 w-[58px]">
+              <HashtagIcon className="h-4 w-4 text-gray-500" />
+            </th>
+          )}
           {fields.map((field) => (
             <th
               key={field._id}
@@ -55,14 +59,13 @@ const TableList: React.FC<ListCardProps> = ({
               highlightItem === item._id ? `highlight-${highlightColor}` : ""
             }
           >
-            <td className="font-mono text-sm text-gray-500 border-b border-slate-200">
-              {formatId(item._id)}
-            </td>
+            {!isMobile && (
+              <td className="font-mono text-sm text-gray-500 border-b border-slate-200">
+                {formatId(item._id)}
+              </td>
+            )}
             {fields.map((field) => (
-              <td
-                key={field._id}
-                className="border-b border-slate-200"
-              >
+              <td key={field._id} className="border-b border-slate-200">
                 <div className="flex items-center h-full">
                   <FieldView
                     field={field}
